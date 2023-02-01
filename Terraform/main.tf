866189ab65f50a97cbb9d6d0cba456470b6dac8a
smmacander <47195384+smmacander@users.noreply.github.com>
2023-02-01 12:13:03 -0500

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -14,3 +14,20 @@ provider "google" {
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
+
+#Compute Instance added below
+
+resource "google_compute_instance" "vm_instance" {
+  name = "terraform-instance"
+  machine_type = "f1-micro"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config{
+    }
+  }
+}
