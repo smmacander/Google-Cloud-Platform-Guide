5c899d576aae77eab22a05817279586435976ad8
smmacander <47195384+smmacander@users.noreply.github.com>
2023-02-01 15:52:18 -0500

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -44,3 +44,22 @@ resource "google_compute_instance" "vm_instance" {
     name = "terraform-static-ip"
   }
 }
+# New resource for the storage bucket our application will use.
+resource "google_storage_bucket" "example_bucket" {
+  name = "<UNIQUE-BUCKET-NAME>"
+  location = "US"
+  website {
+    main_page_suffix = "index.html"
+    not_found_page = "404.html"
+  }
+}
+# Create a new instance that uses the bucket
+resource "google_compute_instance" "another_instance" {
+  # Tells Terraform that this VM instance must be created only after the
+  # storage bucket has been created.
+  depends_on = [google_storage_bucket.example_bucket]
+  name = "terraform-instance-2"
+  machine_type = "f1-micro"
+  boot_disk {
+    initialize_params {
+    
