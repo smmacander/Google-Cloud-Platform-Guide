ce26bf155a7d499ca30fa1d23c3a9554a1ad6840
smmacander <47195384+smmacander@users.noreply.github.com>
2023-02-03 16:15:31 -0500

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -62,4 +62,13 @@ resource "google_compute_instance" "another_instance" {
   machine_type = "f1-micro"
   boot_disk {
     initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+  network_interface {
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+    }
+  }
+}
     
