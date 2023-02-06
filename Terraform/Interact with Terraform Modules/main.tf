022aba5ce8c8fcd494d4f287ed4e3d538732e9b9
smmacander <47195384+smmacander@users.noreply.github.com>
2023-02-06 12:37:12 -0500

    Create main.tf

---
+++ Terraform/Interact with Terraform Modules/main.tf
@@ -0,0 +1,36 @@
+provider "google" {
+  version = "~> 3.45.0"
+}
+provider "null" {
+  version = "~> 2.1"
+}
+module "test-vpc-module" {
+  source       = "terraform-google-modules/network/google"
+  version      = "~> 3.2.0"
+  project_id   = var.project_id
+  network_name = "my-custom-mode-network"
+  mtu          = 1460
+  subnets = [
+    {
+      subnet_name   = "subnet-01"
+      subnet_ip     = "10.10.10.0/24"
+      subnet_region = "us-west1"
+    },
+    {
+      subnet_name           = "subnet-02"
+      subnet_ip             = "10.10.20.0/24"
+      subnet_region         = "us-west1"
+      subnet_private_access = "true"
+      subnet_flow_logs      = "true"
+    },
+    {
+      subnet_name               = "subnet-03"
+      subnet_ip                 = "10.10.30.0/24"
+      subnet_region             = "us-west1"
+      subnet_flow_logs          = "true"
+      subnet_flow_logs_interval = "INTERVAL_10_MIN"
+      subnet_flow_logs_sampling = 0.7
+      subnet_flow_logs_metadata = "INCLUDE_ALL_METADATA"
+    }
+  ]
+}
